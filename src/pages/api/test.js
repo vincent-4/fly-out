6b3dc7c76af5a54af3753eb89f2cb216307b2c90
vincent-4 <vincentzhongy+githubvincent4@gmail.com>
2025-01-18 03:39:32 -0500

    add autocomplete endpoint

--- src/pages/api/test.js
+++ src/pages/api/test.js
@@ -8,6 +8,8 @@ export default function handler(req, res) {
     // make date dynamic
     const mlhURL = 'https://mlh.io/seasons/2023/events';
 
+    let mlhData = [];
+
     const eventLogoIndex = 1;
     const eventNameIndex = 2;
     const eventDateIndex = 3;
@@ -20,47 +22,73 @@ export default function handler(req, res) {
         const numOfHackathons =  dom.window.document.getElementsByClassName('inner').length;
         console.log("total number of hackathons found:", numOfHackathons);
         for (let i = 0; i < numOfHackathons; i++){
-            getProperty(dom, i, eventNameIndex);
-            getProperty(dom, i, eventLogoIndex);
-            getProperty(dom, i, eventDateIndex);
-            getProperty(dom, i, eventLocationIndex);
-            getProperty(dom, i, eventNotesIndex);
+            let hackathonObject = {};
+            hackathonObject['name'] = getProperty(dom, i, eventNameIndex);
+            hackathonObject['logoImageURL'] = getProperty(dom, i, eventLogoIndex);
+            hackathonObject['date'] = getProperty(dom, i, eventDateIndex);
+            hackathonObject['location'] = getProperty(dom, i, eventLocationIndex);
+            hackathonObject['format'] =getProperty(dom, i, eventNotesIndex);
+            mlhData.push(hackathonObject);
+            //console.log('MLHDATA is', mlhData);
         }
+        console.log('MLHDATA is', mlhData);
         
     }).catch(err => {
         console.log(err);
     });
 
+    // res.status(200).json({
+    //     name: 'Hackathons Extracted'
+    // })
     res.status(200).json({
-        name: 'Hackathons Extracted'
+            mlhData
     })
 }
 
 function getProperty(domObject, positions, propertyIndex){
 
+    const mainContainer = domObject.window.document.getElementsByClassName('inner')[positions];
 
-
-    const numberOfAttributes = domObject.window.document.getElementsByClassName('inner')[positions].children.length;
+    const numberOfAttributes = mainContainer.children.length;
 
     // resolve this so it reads hackathons from diversity list as well
     if (numberOfAttributes!=9) {
 
-        console.log("Hackathon Property:", domObject.window.document.getElementsByClassName('inner')[positions].children[propertyIndex].innerHTML);
+        //console.log("Hackathon Property:", domObject.window.document.getElementsByClassName('inner')[positions].children[propertyIndex].innerHTML);
 
         if (propertyIndex == 1) {
-            const urlTag = domObject.window.document.getElementsByClassName('inner')[positions].children[propertyIndex].innerHTML;
+            const urlTag = mainContainer.children[propertyIndex].innerHTML;
             const src = urlTag.slice(urlTag.indexOf("src")).split('"')[1];
             console.log(src);
+            return src;
     
+        }
+
+        else if (propertyIndex == 2) {
+            const name = mainContainer.children[propertyIndex].textContent;
+            return name;
+        }
+
+        else if (propertyIndex == 3) {
+            const date = mainContainer.children[propertyIndex].textContent;
+            return date.trim();
         }
     
         else if (propertyIndex == 6) {
             const cityIndex = 0;
             const StateOrCountryIndex = 1;
-            let cityAndState = domObject.window.document.getElementsByClassName('inner')[positions].children[propertyIndex];
+            let cityAndState = mainContainer.children[propertyIndex];
             console.log(cityAndState.children[cityIndex].textContent);
             console.log(cityAndState.children[StateOrCountryIndex].textContent);
         }
+
+
+        else if (propertyIndex == 7) {
+            const hackathonFormat  = mainContainer.children[propertyIndex];
+            const hackathonFormatText = hackathonFormat.children[0].textContent;
+            return hackathonFormatText;
+        }
+
     }
 
     
